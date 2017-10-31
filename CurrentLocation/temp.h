8396516d612a4f733400229767768cf76beae1f2
Senior mobile, web developer <jangjinmei728@hotmail.com>
2017-10-31 11:45:23 -0500

    Add files via upload

---
+++ CurrentLocation/temp.h
@@ -0,0 +1,15 @@
+//
+//  temp.h
+//  Qibla Compass
+//
+//  Created by Polaris on 30/09/17.
+//  Copyright © 2017 Polaris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define DegreesToRadians(angle) ((angle) / 180.0 * M_PI)
+
+@interface temp : NSObject
+
+@end
---
+++ CurrentLocation/temp.m
@@ -0,0 +1,13 @@
+//
+//  temp.m
+//  Qibla Compass
+//
+//  Created by Polaris on 30/09/17.
+//  Copyright © 2017 Polaris. All rights reserved.
+//
+
+#import "temp.h"
+
+@implementation temp
+
+@end
